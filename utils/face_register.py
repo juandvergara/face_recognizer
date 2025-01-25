be222b2d692875d9216b947a71e50c3835895cea
juandvergara <juandavidelp99@gmail.com>
2025-01-25 18:57:28 -0500

    update functions and document

--- utils/face_register.py
+++ utils/face_register.py
@@ -5,18 +5,18 @@ import csv
 import os
 
 class FaceRegister:
-    def __init__(self, name_file_to_save='known_faces'):
-        print('FaceDetector initialized')
+    def __init__(self, name_file_to_save='known_faces', directory='data'):
+        print('FaceRegister initialized')
+        self.directory = directory
         self.name_file_to_save = name_file_to_save
-        print(f'File to save: {self.name_file_to_save}.csv')
+        print(f'File to save: {self.name_file_to_save}.csv into {self.directory}')
 
-    def save_new_face(self, new_face_encoding, new_name):
-        directory = "data"
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+    def __save_new_face(self, new_face_encoding, new_name):
+
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
         
-        path = os.path.join(directory, self.name_file_to_save + ".csv")
-        print(path)
+        path = os.path.join(self.directory, self.name_file_to_save + ".csv")
         if os.path.exists(path):
             with open(path, 'a', newline='') as csvfile:
                 facewriter = csv.writer(csvfile)
@@ -27,13 +27,20 @@ class FaceRegister:
                 facewriter.writerow(['name', 'encoding'])
                 facewriter.writerow([new_name, new_face_encoding.tolist()])
         print('Face saved!')
+    
+    def register_face(self, total_frames=10, name=''):
+        '''
+        Register a new face into the data file, this method will open \\
+        a webcam and will capture the face of the person to register
 
-    def register_face(self):
-        video_capture = cv2.VideoCapture(0)
-        name = ''
+        :param total_frames: int # Number of frames to capture
+        :param name: str # Name of the person to register
+        '''
+        self.name = name
         counter_frames = 0
-        total_frames = 10
+        self.total_frames = total_frames
 
+        video_capture = cv2.VideoCapture(0)
         while True:
             # Grab a single frame of video
             ret, frame = video_capture.read()
@@ -47,12 +54,12 @@ class FaceRegister:
             cv2.imshow('Video', frame)
 
             if face_locations != []:
-                if name == '': name = input('Enter the name of the person: ')
+                if self.name == '': self.name = input('Enter the name of the person: ')
                 print('Registering face...')
                 face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-                self.save_new_face(face_encodings[0], name)
+                self.__save_new_face(face_encodings[0], name)
                 counter_frames += 1
-                if counter_frames == total_frames: 
+                if counter_frames == self.total_frames: 
                     print('Face registered!')
                     break
             
